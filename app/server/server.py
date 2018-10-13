e510b3eaf97b63ecccc17b59838f9bc81051c654
Bryan Taylor <clearly.b.t@gmail.com>
2018-10-13 16:03:39 -0700

    login and register

--- app/server/server.py
+++ app/server/server.py
@@ -11,7 +11,7 @@ import json
 
 app = Flask(__name__, static_folder="../static", template_folder="../static")
 app.config['MONGO_DBNAME'] = "sdhacks"
-app.config['MONGO_URI'] = "mongodb://hacker:sleepingbag1@ds117758.mlab.com:17758/sdhacks"
+app.config['MONGO_URI'] = "mongodb://hacker:sleepingbag1@ds131763.mlab.com:31763/sdhacks"
 app.secret_key = os.urandom(24)
 socketio = SocketIO(app)
 mongo = PyMongo(app)
@@ -21,6 +21,61 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 connectedUsers = {}
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = mongo.db.users.find_one({"username": user_id})
+    if not user:
+        return None
+    return User(user['_id'])
+
+class User():
+    def __init__(self, username):
+        self.username = username
+        self.email = None
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.username
+
+@app.route('/new-customer', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # construct user
+        # name = request.form['firstname']
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password, method='sha256')
+        requested_user = mongo.db.users.find_one({'username': username}) # searches the data base for the username chosen
+        if requested_user is None:
+            mongo.db.users.insert({'username': username, 'password': hashed_password}) # makes a new user inside data base if non already exits
+            return redirect(url_for('index')) # send back to landing page
+    
+        else:
+            return 'Username has already been taken'
+            
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST']) # sets up the page for registration
+def login():
+    if request.method == 'POST':
+        requested_user = mongo.db.users.find_one({'username': request.form['username']})
+        if requested_user:
+            if check_password_hash(requested_user["password"], request.form['password']):
+                connectedUsers[request.form['username']] = None
+                user = User(username=request.form['username'])
+                login_user(user)
+                return redirect(url_for('home')) # send to page with video functionality
+        return 'Invalid Credentials. Please try again.'
+    return render_template('login.html')  
 
 @app.route("/")
 def index():
